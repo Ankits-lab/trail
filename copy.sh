268a541fb287f082bde3b1060f5b8909536ead75
ANKIT KENE <47526753+keneankit01@users.noreply.github.com>
2021-04-30 14:37:21 +0530

    Update copy.sh

--- copy.sh
+++ copy.sh
@@ -1,4 +1,6 @@
+#!/usr/bin/env bash
 mkdir githubpushable
 cp out/target/product/PL2/HyconOS_*.zip githubpushable
 cd githubpushable
-github-release keneankit01/test v2.5 Hycon*.zip
+ls
+echo github-release keneankit01/test v2.5 Hycon*.zip
