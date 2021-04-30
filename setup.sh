0ad7fc91be58387eafea7975261b508fad2d5c88
ANKIT KENE <47526753+keneankit01@users.noreply.github.com>
2021-04-30 14:24:21 +0530

    Add setup.sh

---
+++ setup.sh
@@ -0,0 +1,9 @@
+mkdir mywork
+cd mywork
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt install -y git git-core
+git config --global user.name "keneankit01"
+git config --global user.email "keneankit01@gmail.com"
+git config --global color.ui false
+echo SETUP DONE STARTING ANDROID SETUP
