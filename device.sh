0e512f54c8471343dea5eb5fd8bb19cc8719fde2
ANKIT KENE <47526753+keneankit01@users.noreply.github.com>
2021-04-30 18:37:53 +0530

    Add correct path

--- device.sh
+++ device.sh
@@ -1,3 +1,4 @@
+cd mywork/aosp
 echo starting clonning 
 git clone https://github.com/keneankit01/hycon_PL2.git device/nokia/PL2
 git clone https://github.com/keneankit01/hycon-common-sdm660.git device/nokia/sdm660-common
