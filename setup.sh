2a5c505770196f322aa1e288fa158a68ef1d10d3
ANKIT KENE <47526753+keneankit01@users.noreply.github.com>
2021-05-01 09:35:55 +0530

    Add a check and dependabcy

--- setup.sh
+++ setup.sh
@@ -9,7 +9,9 @@ sudo apt install -y git git-core
 git config --global user.name "keneankit01"
 git config --global user.email "keneankit01@gmail.com"
 git config --global color.ui false
-each zram enabling
+echo zram enabling
+sudo service elasticsearch start
+sudo service elasticsearch status
 sudo apt-get install git
 git clone --recurse-submodules https://github.com/ecdye/zram-config
 cd zram-config
