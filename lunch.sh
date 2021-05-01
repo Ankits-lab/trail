c1269ebb9a0c1ff1de113c4f7484574871f34403
ANKIT KENE <47526753+keneankit01@users.noreply.github.com>
2021-05-01 18:11:25 +0530

    Update lunch.sh

--- lunch.sh
+++ lunch.sh
@@ -5,7 +5,7 @@ export CCACHE_DIR=/tmp/ccache
 export CCACHE_EXEC=$(which ccache)
 export USE_CCACHE=1
 ccache -M 20G
-ccache -o
+ccache -o compression=true
 lunch aosp_PL2-userdebug
 export SKIP_API_CHECKS=true
 export SKIP_ABI_CHECKS=true
