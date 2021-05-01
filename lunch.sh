54f260e2616d3fb7769247297d81d76ff2afdff9
ANKIT KENE <47526753+keneankit01@users.noreply.github.com>
2021-05-01 16:52:01 +0530

    Update lunch.sh

--- lunch.sh
+++ lunch.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 cd mywork/aosp
 source build/envsetup.sh
+export CCACHE_DIR=/tmp/ccache
+export CCACHE_EXEC=$(which ccache)
+export USE_CCACHE=1
+ccache -M 20G
+ccache -o
 lunch aosp_PL2-userdebug
 export SKIP_API_CHECKS=true
 export SKIP_ABI_CHECKS=true
