97a67bab5aa2696f4dd958e78d1015068a04efc5
ANKIT KENE <47526753+keneankit01@users.noreply.github.com>
2021-05-01 09:29:14 +0530

    Add zram Support

--- setup.sh
+++ setup.sh
@@ -9,4 +9,10 @@ sudo apt install -y git git-core
 git config --global user.name "keneankit01"
 git config --global user.email "keneankit01@gmail.com"
 git config --global color.ui false
+each zram enabling
+sudo apt-get install git
+git clone --recurse-submodules https://github.com/ecdye/zram-config
+cd zram-config
+sudo ./install.bash
+echo check zram enabled or not
 echo SETUP DONE STARTING ANDROID SETUP
